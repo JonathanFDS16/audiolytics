aac52fda036147552bf99c813f47579a987984dc
Jonathan <jonathanfranco799@gmail.com>
2025-04-06 23:24:42 -0400

    Initial Commit

---
+++ Audiolytics/AudiolyticsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudiolyticsApp.swift
+//  Audiolytics
+//
+//  Created by Jonathan Da Silva on 4/6/25.
+//
+
+import SwiftUI
+
+@main
+struct AudiolyticsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AudiolyticsTests/AudiolyticsTests.swift
@@ -0,0 +1,17 @@
+//
+//  AudiolyticsTests.swift
+//  AudiolyticsTests
+//
+//  Created by Jonathan Da Silva on 4/6/25.
+//
+
+import Testing
+@testable import Audiolytics
+
+struct AudiolyticsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
