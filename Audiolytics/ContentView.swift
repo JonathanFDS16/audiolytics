d812da28564d9625516b8a8047d2cf829976048a
Jonathan <jonathanfranco799@gmail.com>
2025-04-20 15:58:51 -0400

    Authentication with PCKE for Spotify. Authentication view also implemented

--- Audiolytics/ContentView.swift
+++ Audiolytics/ContentView.swift
@@ -8,15 +8,64 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showAuth = false
+    @State private var authCode: String?
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 20) {
+            if let code = authCode {
+                Text("Code: \(code)")
+                    .padding()
+            }
+
+            Button("Login with Spotify") {
+                showAuth = true
+            }
         }
-        .padding()
+        .sheet(isPresented: $showAuth) {
+            SpotifyAuthSessionViewController(
+                onSuccess: { code in
+                    authCode = code
+                    showAuth = false
+                    exchangeCodeForToken(code: authCode ?? "")
+                },
+                onCancel: {
+                    showAuth = false
+                }
+            )
+        }
     }
+}
+
+func exchangeCodeForToken(code: String) {
+    guard let verifier = UserDefaults.standard.string(forKey: "code_verifier") else { return }
+    guard code != "" else { return }
+
+    var request = URLRequest(url: URL(string: "https://accounts.spotify.com/api/token")!)
+    request.httpMethod = "POST"
+
+    let params = [
+        "client_id": SpotifyConfig.clientId,
+        "grant_type": "authorization_code",
+        "code": code,
+        "redirect_uri": SpotifyConfig.redirectUri,
+        "code_verifier": verifier
+    ]
+
+    request.httpBody = params
+        .map { "\($0.key)=\($0.value)" }
+        .joined(separator: "&")
+        .data(using: .utf8)
+
+    request.addValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+
+    URLSession.shared.dataTask(with: request) { data, response, error in
+        if let data = data {
+            if let json = try? JSONSerialization.jsonObject(with: data) {
+                print("Received tokens: \(json)")
+            }
+        }
+    }.resume()
 }
 
 #Preview {
---
+++ Audiolytics/SpotifyConfig.swift
@@ -0,0 +1,4 @@
+enum SpotifyConfig {
+    static let clientId = "c5b9203f096a4ba7abcadba5d31def4a"
+    static let redirectUri = "audiolytics://callback"
+}
