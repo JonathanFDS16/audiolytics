4aaeba67dbd9ff75b4681af14ce8beca8ec348ed
Hannah <hjhannah21@gmail.com>
2025-04-24 00:14:05 -0400

    fixed wrapped changes

--- Audiolytics/Views/Wrapped/TopView.swift
+++ Audiolytics/Views/Wrapped/TopView.swift
@@ -13,10 +13,6 @@ struct TopView: View {
     @State private var popularityList: [Int] = []
     @State private var timeFrame: String = "short_term"
     @State private var limit: Int = 10
-<<<<<<< Updated upstream
-=======
-    
->>>>>>> Stashed changes
     
     func fetchTopTracks() {
         SpotifyService().getTopTracks(timeRange: timeFrame, limitNum: limit) { tracks in
@@ -54,68 +50,69 @@ struct TopView: View {
         print(obscurityScore)
     }
     
-    
-<<<<<<< Updated upstream
     var body: some View {
         ScrollView{
-            VStack() {
-                Text("App")
-                
-                HStack(){
+                        VStack() {
+                            Text("Genres")
+                                .font(.title)
+                            
+                            if genreList.isEmpty {
+                                Text("genre loading")
+                                    .onAppear{
+                                        fetchGenres()
+                                    }
+                            } else {
+                                ForEach(genreList, id: \.self) { genre in
+                                    Text(genre)
+                                }
+                            }
+                        }
+                        
+                        Text("Top Tracks")
+                            .font(.title)
+                            .padding(.bottom)
+                        
+            if topTracks.isEmpty {
+                Text("Loading...")
+                    .onAppear {
+                        fetchTopTracks()
+                    }
+            }
+            else {
+                            List(topTracks) { track in
+                                VStack(alignment: .leading, spacing: 4) {
+                                    Text(track.name)
+                                        .font(.headline)
+                                    Text(" by " + track.artists.map { $0.name }.joined(separator: ", "))
+                                }
+                                
+                                .padding(.vertical, 4)
+                            }
+                            .listStyle(.plain)
+                        }
+                    }
+                    .padding()
                     
-                    //do segmented picker instead?
-                    Button("short"){
-                        timeFrame = "short_term"
-                        fetchTopTracks()
-                        fetchTopArtists()
-                    }
-                    Button("med")
-                    {
-                        timeFrame = "medium_term"
-                        fetchTopTracks()
-                        fetchTopArtists()
-                    }
-                    Button("long"){
-                        timeFrame = "long_term"
-                        fetchTopTracks()
-                        fetchTopArtists()
-                    }
-                    /*Picker("Number of entries", selection: $limit)
-                     {
-                     ForEach(1...50, id: \.self) { number in
-                     Text("\(number)")
-                     }
-                     }
-                     pickerStyle(.wheel)
-                     */
-                    Button("10 entries")
-                    {
-                        limit = 10
-                        fetchTopTracks()
-                        fetchTopArtists()
-                    }
-                    
-                }
+                    .frame(height: 350)
+                    .padding()
                 
                 
-                VStack() {
-                    Text("Top Tracks")
+                VStack(alignment: .leading) {
+                    Text("Top Artists")
                         .font(.title)
                         .padding(.bottom)
                     
-                    if topTracks.isEmpty {
+                    if topArtists.isEmpty {
                         Text("Loading...")
                             .onAppear {
-                                fetchTopTracks()
+                                fetchTopArtists()
                             }
                     } else {
-                        List(topTracks) { track in
+                        List(topArtists) { artist in
                             VStack(alignment: .leading, spacing: 4) {
-                                Text(track.name)
+                                Text(artist.name)
                                     .font(.headline)
-                                Text(" by " + track.artists.map { $0.name }.joined(separator: ", "))
                             }
-                            
                             .padding(.vertical, 4)
                         }
                         .listStyle(.plain)
@@ -126,144 +123,10 @@ struct TopView: View {
                 .frame(height: 350)
                 .padding()
             }
-            
-=======
-    func getObscurity(){
-        fetchGenres()
-        var popSum = popularityList.reduce(0, +)
-        var length = popularityList.count
-        var avgPop = Double(popSum)/Double(length)
-        var obscurityScore = (Int)(100.0 - avgPop)
-        print(obscurityScore)
-    }
-    
-    
-    var body: some View {
-        ScrollView{
-        VStack() {
-            Text("App")
-            
-            HStack(){
-                
-                //do segmented picker instead?
-                Button("short"){
-                    timeFrame = "short_term"
-                    fetchTopTracks()
-                    fetchTopArtists()
-                }
-                Button("med")
-                {
-                    timeFrame = "medium_term"
-                    fetchTopTracks()
-                    fetchTopArtists()
-                }
-                Button("long"){
-                    timeFrame = "long_term"
-                    fetchTopTracks()
-                    fetchTopArtists()
-                }
-                /*Picker("Number of entries", selection: $limit)
-                 {
-                 ForEach(1...50, id: \.self) { number in
-                 Text("\(number)")
-                 }
-                 }
-                 pickerStyle(.wheel)
-                 */
-                
-                Button("obscure")
-                {
-                    getObscurity()
-                }
-                
-            }
-            
-            
-            VStack() {
-                Text("Genres")
-                    .font(.title)
-
-                if genreList.isEmpty {
-                    Text("genre loading")
-                        .onAppear{
-                            fetchGenres()
-                        }
-                } else {
-                    ForEach(genreList, id: \.self) { genre in
-                        Text(genre)
-                    }
-                }
-            }
-            
-            Text("Top Tracks")
-                .font(.title)
-                .padding(.bottom)
-            
-            if topTracks.isEmpty {
-                Text("Loading...")
-                    .onAppear {
-                        fetchTopTracks()
-                    }
-            } else {
-                List(topTracks) { track in
-                    VStack(alignment: .leading, spacing: 4) {
-                        Text(track.name)
-                            .font(.headline)
-                        Text(" by " + track.artists.map { $0.name }.joined(separator: ", "))
-                    }
-                    
-                    .padding(.vertical, 4)
-                }
-                .listStyle(.plain)
-            }
-        }
-        .padding()
         
-        .frame(height: 350)
-        .padding()
+        
     }
 
-        
->>>>>>> Stashed changes
-            VStack(alignment: .leading) {
-                Text("Top Artists")
-                    .font(.title)
-                    .padding(.bottom)
-                
-                if topArtists.isEmpty {
-                    Text("Loading...")
-                        .onAppear {
-                            fetchTopArtists()
-                        }
-                } else {
-                    List(topArtists) { artist in
-                        VStack(alignment: .leading, spacing: 4) {
-                            Text(artist.name)
-                                .font(.headline)
-                        }
-                        .padding(.vertical, 4)
-                    }
-                    .listStyle(.plain)
-                }
-            }
-            .padding()
-            
-            .frame(height: 350)
-            .padding()
-        }
-<<<<<<< Updated upstream
-=======
-            
-    
-    
-    
-    
-    
->>>>>>> Stashed changes
-        
-        
-    }
-}
 
     #Preview {
         TopView()
